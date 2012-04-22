a5bf87ea2d0bd9fbadcb545d57fa9d8c06e143eb
pussbb <pussbb@gmail.com>
2012-04-22 17:52:11 +0300

    remove not needed
    Signed-off-by: pussbb <pussbb@gmail.com>

--- application/classes/html.php
+++ application/classes/html.php
@@ -2,25 +2,6 @@
 
 class HTML extends Kohana_HTML {
 
-    public static function tag($name_with_attributes, $enclose = true)
-    {
-        $tag_name = $name_with_attributes[0];
-        unset($name_with_attributes[0]);
-        $result = '<'.$tag_name.HTML::attributes($name_with_attributes);
-        return $enclose ? $result.'/>' : $result.'>';
-    }
-
-    // markup helper
-    public static function button($base_prefix, $label)
-    {
-        $results = array(
-            '<div class="'.$base_prefix.'-left"></div>',
-            '<div class="'.$base_prefix.'-middle">'.$label.'</div>',
-            '<div class="'.$base_prefix.'-right"></div>'
-        );
-        return implode('', $results);
-    }
-
     public static function img($image_url, $attributes = array())
     {
         if (strpos($image_url, '://') === FALSE)
