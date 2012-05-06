cbdda6ada745af41de26b408ff508430a79e1b9c
pussbb <pussbb@gmail.com>
2012-05-06 23:17:31 +0300

    display categories
    Signed-off-by: pussbb <pussbb@gmail.com>

--- application/views/panel/panel/index.php
+++ application/views/panel/panel/index.php
@@ -1,8 +1,53 @@
 <?php
+
+function print_categories($categories)
+{
+    if ( ! $categories)
+        return;
+    echo '<ol>';
+    foreach($categories as $key => $item)
+    {
+        echo '<li data-id="'.$key.'">';
+        echo $item['name'];
+        if ( $item['childs'])
+            print_categories($item['childs']);
+        echo '</li>';
+    }
+    echo '</ol>';
+}
+function build_childs(&$categories, $parent)
+{
+    $reslut = array();
+    foreach($categories as $key => $item)
+    {
+         if ( (int)$item['parent_id'] == $parent)
+         {
+             unset($categories[$key]);
+             $id = $item['id'];
+             $reslut[$id] = array(
+                 'name' => $item['name'],
+                 'childs' =>  build_childs($categories, $id)
+             );
+         }
+    }
+    return $reslut;
+}
+function build_cat_tree($categories)
+{
+    return array(
+        NULL => array(
+            'name' => __('root'),
+            'childs' => build_childs($categories, NULL),
+        ),
+    );
+}
 $current_user = Auth::instance()->current_user();
 $categories = Model_Categories::find_all(array(
     'user_id' => $current_user->id,
 ));
+
+$cat_struct = build_cat_tree($categories->records);
+
 $sites = Model_Sites::find_all( array(
     'user_id' => $current_user->id,
 ));
@@ -63,6 +108,18 @@ $screenshorts = Model_Screenshorts::find_all(array(
             <?php
                 echo __('categories');
             ?> <a class="close" title="hide sidebar">&times;</a>
+            <?php
+                echo '<ul>';
+                    foreach($cat_struct as $key => $item)
+                    {
+                        echo '<li data-id="'.$key.'">';
+                            echo $item['name'];
+                            if ( $item['childs'])
+                                print_categories($item['childs']);
+                        echo '</li>';
+                    }
+                echo '</ul>';
+            ?>
         </div>
         <!--Sidebar content-->
     </div>
