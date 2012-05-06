25845fc63386308e36154a753490340349fadfdb
pussbb <pussbb@gmail.com>
2012-05-06 21:22:33 +0300

    update
    Signed-off-by: pussbb <pussbb@gmail.com>

--- application/views/panel/panel/index.php
+++ application/views/panel/panel/index.php
@@ -1,4 +1,16 @@
-
+<?php
+$current_user = Auth::instance()->current_user();
+$categories = Model_Categories::find_all(array(
+    'user_id' => $current_user->id,
+));
+$sites = Model_Sites::find_all( array(
+    'user_id' => $current_user->id,
+));
+$sites_ids = Arr::pluck($sites->records, 'id' );
+$screenshorts = Model_Screenshorts::find_all(array(
+    'url_id' => $sites_ids
+));
+?>
 <div class="alert alert-block">
     <a class="close" data-dismiss="alert">×</a>
     <h4 class="alert-heading">Summary</h4>
@@ -6,14 +18,33 @@
     <div class="container-fluid">
         <div class="row-fluid">
             <div class="span4">
-                categories: 0. <br/>
-                sites: 0. <br/>
+                <?php
+                 echo Text::auto_p(__('categories').':&nbsp;'. $categories->total_count);
+                 echo Text::auto_p(__('sites').':&nbsp;'. $sites->total_count);
+                ?>
             </div>
             <div >
                 generated screenshorts
-                <div class="progress progress-striped active">
-                    <div class="bar" style="width: 60%;"></div>
-                    <div class="progress-info">40%</div>
+                <?php
+                    $max = $screenshorts->total_count;
+                    $success = 0;
+                        foreach($screenshorts->records as $item)
+                        {
+                            $status = Arr::get($item, 'status', Model_Screenshorts::STATUS_FAIL);
+                            if ($status == Model_Screenshorts::STATUS_GENERATED)
+                                ++$success;
+                        }
+                    $progress = (100 / $max) *$success;
+                    $progress = round($progress, 0);
+                    $progress_info_class =  'progress-success';
+                    if ( $progress < 50)
+                        $progress_info_class = 'progress-danger';
+                    if ( $progress >= 50 && $progress < 100)
+                        $progress_info_class = 'progress-warning';
+                ?>
+                <div class="progress <?php echo $progress_info_class;?> progress-striped active">
+                    <div class="bar" style="width: <?php echo $progress;?>%;"></div>
+                    <div class="progress-info"> <?php echo $progress;?>%</div>
                 </div>
             </div>
         </div>
